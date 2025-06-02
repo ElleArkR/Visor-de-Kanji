41a7dc91c0185241b67faa0798aa0e293333daac
ElleArkR <erneymax@gmail.com>
2025-06-02 18:04:24 -0500

    Delete kanji_project directory

--- kanji_project/config.py
+++
@@ -1,7 +0,0 @@
-# This file will contain configuration settings for the Flask application.
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///kanji.db'
-    # Add other configuration variables as needed
-    # For example, a secret key for sessions:
-    # SECRET_KEY = 'your_secret_key'
